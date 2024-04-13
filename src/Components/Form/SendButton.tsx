e5c92bbeb44cfaed17027a54e0112f38913954bb
effedici <26420747+dicaeffe@users.noreply.github.com>
2024-04-13 16:26:23 +0200

    feat: notification snackbar

--- src/Components/Form/SendButton.tsx
+++ src/Components/Form/SendButton.tsx
@@ -1,9 +1,13 @@
-import { Button } from '@mui/material'
+import { Button, IconButton, Snackbar } from '@mui/material'
 import SendIcon from '@mui/icons-material/Send';
 import MyPreConfig from '../BrickUtils/MyPreConfig';
+import CloseIcon from '@mui/icons-material/Close';
+import React, { useState } from 'react';
 
 function SendButton(props: Send) {
-
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [messageSnackbar, setMessageSnackbar] = useState(false);
+    
     const onClick = () => {
         props.body.map(element => {
             alert(element.toString());
@@ -15,8 +19,46 @@ function SendButton(props: Send) {
                 },
                 body: JSON.stringify(element)
             })
+            .then(response => {
+              if (response.ok) {
+                setMessageSnackbar(true);
+                setOpenSnackbar(true);
+                throw new Error(response.statusText)
+              } else {
+                setMessageSnackbar(false);
+                setOpenSnackbar(true);
+              }
+            })
+            .catch(() => {
+                setMessageSnackbar(false);
+                setOpenSnackbar(true);
+            })
         })
     };
+
+    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setOpenSnackbar(false);
+    };
+    
+    const action = (
+      <React.Fragment>
+        <Button color="secondary" size="small" onClick={handleClose}>
+          vabbé
+        </Button>
+        <IconButton
+          size="small"
+          aria-label="close"
+          color="inherit"
+          onClick={handleClose}
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
+      </React.Fragment>
+    );
 
     return (
         <div>
@@ -25,8 +67,16 @@ function SendButton(props: Send) {
                 endIcon={<SendIcon />}
                 onClick={onClick}
                 >
-                Send
+                Invia
             </Button>
+            
+            <Snackbar
+                open={openSnackbar}
+                autoHideDuration={3000}
+                onClose={handleClose}
+                message={messageSnackbar ? "Inviato" : "Errore"}
+                action={action}
+            />
         </div>
     )
 }
