76808822e6d9bb83e8070c78174195e7c9bfb1d9
effedici <26420747+dicaeffe@users.noreply.github.com>
2025-06-30 15:24:36 +0200

    fix description auto-update issue

--- src/Components/PaymentForm/Fields/PaymentDescription.tsx
+++ src/Components/PaymentForm/Fields/PaymentDescription.tsx
@@ -1,33 +1,36 @@
-import { TextField } from '@mui/material';
-import PaymentData from '../Model/PaymentData';
+import { TextField } from "@mui/material";
+import PaymentData from "../Model/PaymentData";
 
-function PaymentDescription(props:{paymentData:PaymentData, onChange:(d: PaymentData) => void}) {
-    const label="Descrizione"
-    const id="descrizione-text"
-    const helper="helper da valorizzare"
-    
-    const handleFormChange = (e: { target: { value: any; }; }) => {
-        let newData:PaymentData = props.paymentData.clone();
-        newData.description = e.target.value;
-        console.info(newData);
-        props.onChange(newData);
-    }
+function PaymentDescription(props: {
+  paymentData: PaymentData;
+  onChange: (d: PaymentData) => void;
+}) {
+  const label = "Descrizione";
+  const id = "descrizione-text";
+  const helper = "helper da valorizzare";
 
-    return (
-        <div>
-            <TextField
-                label={label}
-                required
-                id={id}
-                helperText={helper}
-                defaultValue={props.paymentData.description}
-                onChange={event => handleFormChange(event)}
-                type="text"
-                margin="normal"
-                fullWidth 
-            />
-        </div>
-    )
+  const handleFormChange = (e: { target: { value: any } }) => {
+    let newData: PaymentData = props.paymentData.clone();
+    newData.description = e.target.value;
+    console.info(newData);
+    props.onChange(newData);
+  };
+
+  return (
+    <div>
+      <TextField
+        label={label}
+        required
+        id={id}
+        helperText={helper}
+        value={props.paymentData.description}
+        onChange={(event) => handleFormChange(event)}
+        type="text"
+        margin="normal"
+        fullWidth
+      />
+    </div>
+  );
 }
 
-export default PaymentDescription
+export default PaymentDescription;
