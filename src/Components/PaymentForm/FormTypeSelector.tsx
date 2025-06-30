46be10e7f42d22a41993178c6d658089532e91ee
effedici <26420747+dicaeffe@users.noreply.github.com>
2025-06-30 14:19:30 +0200

    add tooltip

--- src/Components/PaymentForm/FormTypeSelector.tsx
+++ src/Components/PaymentForm/FormTypeSelector.tsx
@@ -1,84 +1,94 @@
-import { Chip, Avatar, Card, Stack, Divider, CardContent } from '@mui/material';
-import PaymentData from './Model/PaymentData';
-import Transaction from './Model/Transaction';
-import { useState } from 'react';
-import PaymentDate from './Fields/PaymentDate';
-import PaymentDescription from './Fields/PaymentDescription';
-import PaymentCategory from './Fields/PaymentCategory';
-import PaymentChannel from './Fields/PaymentChannel';
-import PaymentAmount from './Fields/PaymentAmount';
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
-import Edenred from '/images/avatars/Edenred.png';
-import SendButton from './BrickUtils/SendButton';
+import { Chip, Avatar, Card, Stack, Divider, CardContent } from "@mui/material";
+import PaymentData from "./Model/PaymentData";
+import Transaction from "./Model/Transaction";
+import { useState } from "react";
+import PaymentDate from "./Fields/PaymentDate";
+import PaymentDescription from "./Fields/PaymentDescription";
+import PaymentCategory from "./Fields/PaymentCategory";
+import PaymentChannel from "./Fields/PaymentChannel";
+import PaymentAmount from "./Fields/PaymentAmount";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import Edenred from "/images/avatars/Edenred.png";
+import SendButton from "./BrickUtils/SendButton";
+import InfoIcon from "@mui/icons-material/Info";
+import Tooltip from "@mui/material/Tooltip";
 
 function FormTypeSelector() {
-  const label = "FormTypeSelector"
+  const label = "FormTypeSelector";
   const [paymentData, setPaymentData] = useState(new PaymentData(new Date()));
 
-  const values = [{
-    key: 0,
-    name: "Introiti",
-    avatar: "I",
-    value: new PaymentData(new Date(), "payroll", "", [
-      new Transaction("out", "ccWebank", 0.00)
-    ])
-  }, {
-    key: 1,
-    name: "BP",
-    avatar: "B",
-    value: new PaymentData(new Date(), "ciboUfficio", "", [
-      new Transaction("buoniPasto", "out", 16.00)
-    ])
-  }, {
-    key: 2,
-    name: "BP+Cred",
-    avatar: "B+C",
-    value: new PaymentData(new Date(), "ciboUfficio", "Pranzo da ", [
-      new Transaction("buoniPasto", "out", 16.00),
-      new Transaction("crediti", "out", 10.00)
-    ])
-  }, {
-    key: 3,
-    name: "BP2Cred",
-    avatar: "B2c",
-    value: new PaymentData(new Date(), "ciboUfficio", "Pranzo da ", [
-      new Transaction("buoniPasto", "out", 16.00),
-      new Transaction("buoniPasto", "crediti", 10.00)
-    ])
-  }, {
-    key: 4,
-    name: "BP2Cr-Cred",
-    avatar: "B2cC",
-    value: new PaymentData(new Date(), "ciboUfficio", "Pranzo da ", [
-      new Transaction("buoniPasto", "out", 10.00),
-      new Transaction("buoniPasto", "crediti", 6.00),
-      new Transaction("crediti", "out", 10.00)
-    ])
-  }, {
-    key: 5,
-    name: "SpesaBP",
-    avatar: Edenred,
-    value: new PaymentData(new Date(), "spesaCasa", "Spesa da ", [
-      new Transaction("buoniPasto", "out", 10.00),
-      new Transaction("ccWebank", "out", 10.00)
-    ])
-  }, {
-    key: 6,
-    name: "SpesaBP+Ill",
-    avatar: Edenred,
-    value: new PaymentData(new Date(), "spesaCasa", "Spesa da ", [
-      new Transaction("buoniPasto", "out", 10.00),
-      new Transaction("ccIllimity", "out", 10.00)
-    ])
-  }, {
-    key: 7,
-    name: "Spesa",
-    avatar: "S",
-    value: new PaymentData(new Date(), "spesaCasa", "Spesa da ", [
-      new Transaction("ccWebank", "out", 10.00)
-    ])
-  }
-  ]
+  const values = [
+    {
+      key: 0,
+      name: "Introiti",
+      avatar: "I",
+      value: new PaymentData(new Date(), "payroll", "", [
+        new Transaction("out", "ccWebank", 0.0),
+      ]),
+    },
+    {
+      key: 1,
+      name: "BP",
+      avatar: "B",
+      value: new PaymentData(new Date(), "ciboUfficio", "", [
+        new Transaction("buoniPasto", "out", 16.0),
+      ]),
+    },
+    {
+      key: 2,
+      name: "BP+Cred",
+      avatar: "B+C",
+      value: new PaymentData(new Date(), "ciboUfficio", "Pranzo da ", [
+        new Transaction("buoniPasto", "out", 16.0),
+        new Transaction("crediti", "out", 10.0),
+      ]),
+    },
+    {
+      key: 3,
+      name: "BP2Cred",
+      avatar: "B2c",
+      value: new PaymentData(new Date(), "ciboUfficio", "Pranzo da ", [
+        new Transaction("buoniPasto", "out", 16.0),
+        new Transaction("buoniPasto", "crediti", 10.0),
+      ]),
+    },
+    {
+      key: 4,
+      name: "BP2Cr-Cred",
+      avatar: "B2cC",
+      value: new PaymentData(new Date(), "ciboUfficio", "Pranzo da ", [
+        new Transaction("crediti", "out", 10.0),
+        new Transaction("buoniPasto", "out", 10.0),
+        new Transaction("buoniPasto", "crediti", 6.0),
+      ]),
+    },
+    {
+      key: 5,
+      name: "SpesaBP",
+      avatar: Edenred,
+      value: new PaymentData(new Date(), "spesaCasa", "Spesa da ", [
+        new Transaction("buoniPasto", "out", 10.0),
+        new Transaction("ccWebank", "out", 10.0),
+      ]),
+    },
+    {
+      key: 6,
+      name: "SpesaBP+Ill",
+      avatar: Edenred,
+      value: new PaymentData(new Date(), "spesaCasa", "Spesa da ", [
+        new Transaction("buoniPasto", "out", 10.0),
+        new Transaction("ccIllimity", "out", 10.0),
+      ]),
+    },
+    {
+      key: 7,
+      name: "Spesa",
+      avatar: "S",
+      value: new PaymentData(new Date(), "spesaCasa", "Spesa da ", [
+        new Transaction("ccWebank", "out", 10.0),
+      ]),
+    },
+  ];
 
   const onFormChange = (newData: PaymentData) => {
     //if(selectedValues) setPaymentData(selectedValues => selectedValues);
@@ -86,12 +96,29 @@ function FormTypeSelector() {
     console.info("---------New Value of paymentData");
     console.info(newData);
     console.info(paymentData);
+  };
+
+  function getTip(paymentData: PaymentData, index: number): string {
+    if ("crediti" === paymentData.getFieldValue("outgoingFund", index)) {
+      return "es. Abbuono";
+    } else if (
+      "buoniPasto" === paymentData.getFieldValue("outgoingFund", index) &&
+      "out" === paymentData.getFieldValue("incomingFund", index)
+    ) {
+      return "es. cibo preso";
+    } else if (
+      "buoniPasto" === paymentData.getFieldValue("outgoingFund", index) &&
+      "crediti" === paymentData.getFieldValue("incomingFund", index)
+    ) {
+      return "es. Pareggiatore (togliere l'abbuono)";
+    }
+    return "---";
   }
 
   return (
     <div>
       <p>{label}</p>
-      {values.map(v =>
+      {values.map((v) => (
         <Chip
           key={v.key}
           avatar={<Avatar alt={v.name} src={v.avatar} />}
@@ -99,10 +126,11 @@ function FormTypeSelector() {
           variant="outlined"
           color="secondary"
           onClick={() => {
-            console.info('You clicked the Chip.');
-            onFormChange(v.value)
-          }} />
-      )}
+            console.info("You clicked the Chip.");
+            onFormChange(v.value);
+          }}
+        />
+      ))}
       <Divider orientation="horizontal" variant="middle" flexItem />
       <br />
       <PaymentCategory paymentData={paymentData} onChange={onFormChange} />
@@ -113,40 +141,91 @@ function FormTypeSelector() {
       <br />
 
       {paymentData.transactions?.map((t, index) => {
-        console.log(t)
+        console.log(t);
         return (
           <div key={index}>
             <Card>
-              <CardContent color='primary'>
-                <Stack spacing={{ xs: 1, sm: 2 }} direction="row" useFlexGap flexWrap="wrap" justifyContent="center" alignItems="center">
-                  <PaymentChannel paymentData={paymentData} onChange={onFormChange} index={index} field={"outgoingFund"} />
+              <CardContent color="primary">
+                <Stack
+                  spacing={{ xs: 1, sm: 2 }}
+                  direction="row"
+                  useFlexGap
+                  flexWrap="wrap"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <PaymentChannel
+                    paymentData={paymentData}
+                    onChange={onFormChange}
+                    index={index}
+                    field={"outgoingFund"}
+                  />
                   <KeyboardDoubleArrowRightIcon />
-                  <PaymentChannel paymentData={paymentData} onChange={onFormChange} index={index} field={"incomingFund"} />
+                  <PaymentChannel
+                    paymentData={paymentData}
+                    onChange={onFormChange}
+                    index={index}
+                    field={"incomingFund"}
+                  />
                   <Divider orientation="vertical" variant="middle" flexItem />
-                  <PaymentAmount paymentData={paymentData} onChange={onFormChange} index={index} />
+                  <PaymentAmount
+                    paymentData={paymentData}
+                    onChange={onFormChange}
+                    index={index}
+                  />
+                  <Divider orientation="vertical" variant="middle" flexItem />
+                  <Tooltip title={getTip(paymentData, index)}>
+                    <InfoIcon />
+                  </Tooltip>
+
+                  {/*<>
+                    {"crediti" ===
+                      paymentData.getFieldValue("outgoingFund", index) && (
+                      <p>es. Abbuono</p>
+                    )}
+                    {"buoniPasto" ===
+                      paymentData.getFieldValue("outgoingFund", index) &&
+                      "out" ===
+                        paymentData.getFieldValue("incomingFund", index) && (
+                        <p>es. cibo preso</p>
+                      )}
+                    {"buoniPasto" ===
+                      paymentData.getFieldValue("outgoingFund", index) &&
+                      "crediti" ===
+                        paymentData.getFieldValue("incomingFund", index) && (
+                        <p>es. Pareggiatore (togliere l'abbuono)</p>
+                      )}
+                  </>*/}
                 </Stack>
               </CardContent>
             </Card>
             <br />
           </div>
-        )
+        );
       })}
       <SendButton paymentData={paymentData} />
       <br />
 
       <Divider orientation="horizontal" variant="middle" flexItem />
-      <strong>Riepilogo</strong><br />
+      <strong>Riepilogo</strong>
+      <br />
       <table>
         <tr>
-          <td><strong>Data</strong></td>
+          <td>
+            <strong>Data</strong>
+          </td>
           <td>{paymentData.transactionDate.toISOString()}</td>
         </tr>
         <tr>
-          <td><strong>Cat.</strong></td>
+          <td>
+            <strong>Cat.</strong>
+          </td>
           <td>{paymentData.category}</td>
         </tr>
         <tr>
-          <td><strong>Desc.</strong></td>
+          <td>
+            <strong>Desc.</strong>
+          </td>
           <td>{paymentData.description}</td>
         </tr>
         <tr>
@@ -156,22 +235,24 @@ function FormTypeSelector() {
               {paymentData.transactions?.map((value, index) => {
                 return (
                   <tr>
-                    <td><strong>{index}</strong></td>
+                    <td>
+                      <strong>{index}</strong>
+                    </td>
                     <td>{value.outgoingFund}</td>
                     <td>{"➡️"}</td>
                     <td>{value.incomingFund}</td>
                     <td>{value.amount + "€"}</td>
                   </tr>
-                )
+                );
               })}
             </table>
           </td>
         </tr>
       </table>
-      
+
       <Divider orientation="horizontal" variant="middle" flexItem />
     </div>
-  )
+  );
 }
 
-export default FormTypeSelector
+export default FormTypeSelector;
