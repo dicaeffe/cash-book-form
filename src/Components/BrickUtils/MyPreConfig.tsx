ce8b5bb413f64569d39039dc5636bbdfe672320b
effedici <26420747+dicaeffe@users.noreply.github.com>
2025-10-19 20:28:08 +0200

    lint

--- src/Components/BrickUtils/MyPreConfig.tsx
+++ src/Components/BrickUtils/MyPreConfig.tsx
@@ -1,55 +1,68 @@
 class MyPreConfig {
-	transactionDate: Date;
-	category?: string | null;
-	description?: string | null;
-	outgoingFund?: string | null;
-	incomingFund?: string | null;
-	amount?: number | null;
+  transactionDate: Date;
+  category?: string | null;
+  description?: string | null;
+  outgoingFund?: string | null;
+  incomingFund?: string | null;
+  amount?: number | null;
 
-	constructor(
-			date: Date | null,
-			category?: string | null,
-			description?: string | null,
-			outgoingFund?: string | null,
-			incomingFund?: string | null,
-			amount?: number | null) {
-		this.transactionDate = date ? date : new Date();
-		this.category = category;
-		this.description = description;
-		this.outgoingFund = outgoingFund;
-		this.incomingFund = incomingFund;
-		this.amount = amount;
-	}
+  constructor(
+    date: Date | null,
+    category?: string | null,
+    description?: string | null,
+    outgoingFund?: string | null,
+    incomingFund?: string | null,
+    amount?: number | null
+  ) {
+    this.transactionDate = date ? date : new Date();
+    this.category = category;
+    this.description = description;
+    this.outgoingFund = outgoingFund;
+    this.incomingFund = incomingFund;
+    this.amount = amount;
+  }
 
-	public update(formData: MyPreConfig) {
-		formData.transactionDate = this.transactionDate ? this.transactionDate : formData.transactionDate
+  public update(formData: MyPreConfig) {
+    formData.transactionDate = this.transactionDate
+      ? this.transactionDate
+      : formData.transactionDate;
 
-		if (this.category) {
-			formData.category = this.category
-		}
-		if (this.description) {
-			formData.description = this.description
-		}
-		if (this.outgoingFund) {
-			formData.outgoingFund = this.outgoingFund
-		}
-		if (this.description) {
-			formData.incomingFund = this.incomingFund
-		}
-		if (this.description) {
-			formData.amount = this.amount
-		}
-	}
+    if (this.category) {
+      formData.category = this.category;
+    }
+    if (this.description) {
+      formData.description = this.description;
+    }
+    if (this.outgoingFund) {
+      formData.outgoingFund = this.outgoingFund;
+    }
+    if (this.incomingFund) {
+      formData.incomingFund = this.incomingFund;
+    }
+    if (this.amount) {
+      formData.amount = this.amount;
+    }
+  }
 
-	public toString():string {
-		return "\n"
-		+ this.transactionDate.toISOString() + "\n"
-		+ this.category + "\n"
-		+ this.description + "\n"
-		+ "---" + "\n"
-		+ this.outgoingFund + " ➡️ " + this.incomingFund + "\n"
-		+ this.amount + "€\n"
-	}
+  public toString(): string {
+    return (
+      "\n" +
+      this.transactionDate.toISOString() +
+      "\n" +
+      this.category +
+      "\n" +
+      this.description +
+      "\n" +
+      "---" +
+      "\n" +
+      this.outgoingFund +
+      " ➡️ " +
+      this.incomingFund +
+      "\n" +
+      this.amount +
+      "€\n"
+    );
+  }
 }
 
-export default MyPreConfig
+export default MyPreConfig;
